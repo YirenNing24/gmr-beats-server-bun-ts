211c6006562d52ed58fcec1dbf8ea2d5738e818a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-29 23:46:51 +0800

    updates

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { APP_PORT } from "./config/constants";
 
 const port: number = APP_PORT;
 
-app.listen({ port }, () => {
+app.listen({ port, hostname: '0.0.0.0' }, () => {
     console.log(`Beats admin server is running on port http://localhost:${port}/`);
 });
