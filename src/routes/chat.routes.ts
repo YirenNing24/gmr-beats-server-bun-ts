5d687c0ad6d0534795a205e6c636dc0fe6ed8151
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-31 15:23:12 +0800

    updates

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -1,16 +1,24 @@
+
+// import ChatService from "../game.services/chat.service";
+
 import Elysia from "elysia";
 
-const chat = (app: Elysia) => {
-  app.ws('/api/chats/:room', {
-    message(ws, message) {
-      // Get the value of the :room parameter from the URL
-      //@ts-ignore
-      const room = ws.request.params.room;
-      console.log(`Received message for room: ${room}`);
-      
-      ws.send(message);
-    }
-  });
-};
 
-export default chat;
+
+ const chat = (app: Elysia) => {
+  
+   app.ws('/api/chats/:room', {
+    //@ts-ignore
+    message(ws, message, route) {
+        const roomParam = route.params.room;
+
+        // Now, you can use the 'roomParam' as needed
+        console.log('Room parameter:', roomParam);
+        ws.send(message)
+    }
+})
+
+//   done();
+ };
+
+ export default chat;
