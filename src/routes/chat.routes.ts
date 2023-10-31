476b739eb538b23850009eff1b763eea50aefeda
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-31 19:38:49 +0800

    updates

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -10,11 +10,8 @@ import Elysia from "elysia";
    app.ws('/api/chats/:room', {
     //@ts-ignore
     message(ws, message, route) {
-        const roomParam = route.params.room;
-
-        // Now, you can use the 'roomParam' as needed
-        console.log('Room parameter:', roomParam);
         ws.send(message)
+        console.log(route)
     }
 })
 
