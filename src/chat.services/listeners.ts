197234331d50ce7f819b0ed64ca7f6a4129cec29
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-11-06 00:33:40 +0800

    updates

--- src/chat.services/listeners.ts
+++ src/chat.services/listeners.ts
@@ -10,29 +10,26 @@ import { RDB_DATABASE } from "../config/constants";
 const listenAll = () => {
  const ws: WebSocketServer = new WebSocketServer()
     ws.on("connection", (client) => {
-        const ws: WebSocketServer = new WebSocketServer()
-           ws.on("connection", (client) => {
-               client.addEventListener("message", async (message) => {
-                   try{
-                       const parsedMessage = JSON.parse(message.toString());
-                       if (parsedMessage && parsedMessage.message !== null && parsedMessage.message !== "") {
-                           const connection: rt.Connection = await getRethinkDB();
-                           // Insert the parsed message into the "chats" table with a timestamp
-                    await rt.db(RDB_DATABASE)
-                      .table("chats")
-                      .insert({
-                        ...parsedMessage,
-                        ts: Date.now(),
-                      })
-                      .run(connection);
-                    }
-            } catch (error: any) {
-                throw Error("Error processing message:", error)
+        client.addEventListener("message", async (message) => {
+            try{
+            const parsedMessage = JSON.parse(message.toString());
+            if (parsedMessage && parsedMessage.message !== null && parsedMessage.message !== "") {
+            const connection: rt.Connection = await getRethinkDB();
+            // Insert the parsed message into the "chats" table with a timestamp
+            await rt.db(RDB_DATABASE)
+                .table("chats")
+                .insert({
+                ...parsedMessage,
+                ts: Date.now(),
+                })
+                .run(connection);
             }
-        })
+        } catch (error: any) {
+          throw Error("Error processing message:", error)
+        }
+    })
 
-    })
-    })
+ })
 } ;
 
 
