9a56ee245e7d30ab9f46812cdcc4ce2b73073e47
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-11-05 22:31:10 +0800

    updates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,17 @@
+import Elysia from 'elysia'
+
 import auth from './auth.routes'
 import store from './store.routes'
 import profile from './profile.routes'
 import inventory from './inventory.route'
 import chat from './chat.routes'
 
-import Elysia from 'elysia'
 
 // import { insertChats } from '../game.services/chat.service';
 // Routes Registration Function
 // This function is responsible for registering all the routes and services within the application.
 
-const routes = (app: Elysia) => {
+const routes = (app: any) => {
 
     //@ts-ignore
     app.use(auth)
