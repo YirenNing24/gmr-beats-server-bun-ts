478a8a16e98583b3d26c1b0c4b9b68d8dd2c405b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-11-06 02:31:10 +0800

    updates

--- src/chat.services/listeners.ts
+++ src/chat.services/listeners.ts
@@ -15,7 +15,7 @@ interface Message {
 const listenAll = async (message: Message): Promise<void> => {
     console.log(message, "listen allll")
   try {
-    const parsedMessage = JSON.parse(message.toString());
+    // const parsedMessage = JSON.parse(message.toString());
 
     // Check if the message is not blank or null
 
