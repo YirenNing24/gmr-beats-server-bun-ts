2ca3fbe3691ff35865ef2570bf95d319c5b00e30
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-11-06 22:39:53 +0800

    updates

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -16,60 +16,76 @@ interface Message {
 
  const chat = (app: Elysia): void => {
 
+
    app.ws('/api/chats/:room', { async open(ws) {
+        //@ts-ignore
+        const room: string = ws.data.params.room
+        const chatService: ChatService = new ChatService()
+        chatService.chatRoom(room, ws)
+        
+    }, message(ws, message) {
+        const msg = message as Message
+        console.log(msg)
+        listenAll(msg)
+    }})
+
+//    app.ws('/api/chats/:room', 
+//    { async open(ws) {
     
-        try {
-            //@ts-ignore
-            // const authorizationHeader: string | null = ws.data.headers.authorization;
-            // if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-            //   ws.close()
-            //   return 
-            // }
+//         try {
+//             //@ts-ignore
+//             // const authorizationHeader: string | null = ws.data.headers.authorization;
+//             // if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+//             //   ws.close()
+//             //   return 
+//             // }
             
-            // const jwtToken: string | null = authorizationHeader.substring(7);
-            // // Verify the JWT token using 'jsonwebtoken' with options
-            // const decodedToken = await ws.data.jwt.verify(jwtToken);
-            // const msg = message as Message
-            // ws.send(message)
-            // await listenAll(msg)
-            const room: string = ws.data.params.room
-            const chatService: ChatService = new ChatService()
-            chatService.chatRoom(room, ws)
+//             // const jwtToken: string | null = authorizationHeader.substring(7);
+//             // // Verify the JWT token using 'jsonwebtoken' with options
+//             // const decodedToken = await ws.data.jwt.verify(jwtToken);
+//             // const msg = message as Message
+//             // ws.send(message)
+//             // await listenAll(msg)
+        
+//             const room: string = ws.data.params.room
+//             const chatService: ChatService = new ChatService()
+//             chatService.chatRoom(room, ws)
 
-        } catch (error) {
-            ws.send(error)
-            ws.close()
-            // You can send an error response to the client if needed.
-            // ws.send('Error: JWT verification failed');
-        }}
-    })
-    .ws('/api/chats/:room', { message(ws, message) {
-        try {
-            console.log(message)
-            //@ts-ignore
-            // const authorizationHeader: string | null = ws.data.headers.authorization;
-            // if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-            //   ws.close()
-            //   return 
-            // }
+//         } catch (error) {
+//             ws.send(error)
+//             ws.close()
+//             // You can send an error response to the client if needed.
+//             // ws.send('Error: JWT verification failed');
+//         }}
+//     })
+    
+//     .ws('/api/chats/:room', { message(ws, message) {
+//         try {
+//             console.log(message)
+//             //@ts-ignore
+//             // const authorizationHeader: string | null = ws.data.headers.authorization;
+//             // if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+//             //   ws.close()
+//             //   return 
+//             // }
             
-            // const jwtToken: string | null = authorizationHeader.substring(7);
-            // // Verify the JWT token using 'jsonwebtoken' with options
-            // const decodedToken = await ws.data.jwt.verify(jwtToken);
-            // const msg = message as Message
-            // ws.send(message)
-            // await listenAll(msg)
-            const msg = message as Message
-            console.log(msg)
-            listenAll(msg)
+//             // const jwtToken: string | null = authorizationHeader.substring(7);
+//             // // Verify the JWT token using 'jsonwebtoken' with options
+//             // const decodedToken = await ws.data.jwt.verify(jwtToken);
+//             // const msg = message as Message
+//             // ws.send(message)
+//             // await listenAll(msg)
+//             const msg = message as Message
+//             console.log(msg)
+//             listenAll(msg)
 
-        } catch (error) {
-            ws.send(error)
-            ws.close()
-            // You can send an error response to the client if needed.
-            // ws.send('Error: JWT verification failed');
-        }}
-    })
+//         } catch (error) {
+//             ws.send(error)
+//             ws.close()
+//             // You can send an error response to the client if needed.
+//             // ws.send('Error: JWT verification failed');
+//         }}
+//     })
 
 //   done();
 };
