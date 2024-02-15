63bca707d9db66b3a4187018631fc9a45237c563
YirenNing24 <paulanthonyarriola@gmail.com>
2024-02-16 02:30:53 +0800

    updates

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -16,35 +16,44 @@ import TimeService from "../game.services/time.service"
 import { JWT_SECRET } from "../config/constants"
 import ValidationError from "../outputs/validation.error"
 
-
- const chat = (app: Elysia): void => {
-   app.ws('/api/ws', { 
+const chat = (app: Elysia): void => {
+  app.ws('/api/ws', {
     async open(ws) {
+      try {
         //@ts-ignore
         const room: string = 'all'
         const authorizationHeader: string = ws.data.headers.authorization || ""
+        
         if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
           throw new ValidationError('jwt issue', '')
         }
 
         const jwtToken: string = authorizationHeader.substring(7);
-        const decodedToken: string |JwtPayload = jwt.verify(jwtToken, JWT_SECRET)
+        const decodedToken: string | JwtPayload = jwt.verify(jwtToken, JWT_SECRET)
         const { userName } = decodedToken as { userName: string };
 
         //@ts-ignore
         const chatService: ChatService = new ChatService(ws)
         chatService.chatRoom(room, userName)
-        ws?.subscribe('all')
-        },
+        ws?.subscribe('all');
+      } catch (error: any) {
+        return error
+      }
+    },
     async message(ws, message) {
-      const newMessage: any = message as NewMessage
-      
-      newMessage?.roomId && await insertChats(newMessage);
-      //@ts-ignore
-      const timeService: TimeService = new TimeService(ws)
-      newMessage?.timestamp && await timeService.getServerDateTime(newMessage)
+      try {
+        const newMessage: any = message as NewMessage
+        
+        newMessage?.roomId && (await insertChats(newMessage));
+        //@ts-ignore
+        const timeService: TimeService = new TimeService(ws)
+        newMessage?.timestamp && (await timeService.getServerDateTime(newMessage));
+      } catch (error: any) {
+        console.error('Error in WebSocket message event:', error);
+        return error
+      }
     }
-    })
+  });
 };
 
- export default chat;
+export default chat;
