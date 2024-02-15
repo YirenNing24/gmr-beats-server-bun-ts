fa09bce1156f1f45bb302c9f6c684ee898203ad1
YirenNing24 <paulanthonyarriola@gmail.com>
2024-02-15 22:34:15 +0800

    updates

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import { APP_PORT } from "./config/constants";
 
 
 const port: number = APP_PORT;
-const hostname: string = 'localhost'
 
 app.listen({ port }, () => {
-    console.log(`Beats admin server is running on port http://${hostname}:${port}/`);
+    console.log`Runnning at: ${app.server?.url}`
 });
