2d96c7002ac4852e0beb72c64bafefd491bdd2e3
YirenNing24 <paulanthonyarriola@gmail.com>
2024-02-15 22:06:08 +0800

    updates

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { APP_PORT } from "./config/constants";
 
 
 const port: number = APP_PORT;
-const hostname: string = 'gmetarave.art'
+const hostname: string = 'localhost'
 
 app.listen({ port }, () => {
     console.log(`Beats admin server is running on port http://${hostname}:${port}/`);
