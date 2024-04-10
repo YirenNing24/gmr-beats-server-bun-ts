49b6be5f2d9606c607623534f582f6e71884f470
YirenNing24 <paulanthonyarriola@gmail.com>
2024-04-11 00:15:32 +0800

    updates

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -25,10 +25,6 @@ export const KEYDB_PASSWORD: string | undefined = process.env.KEYDB_PASSWORD;
 export const KEYDB_PORT: string | undefined = process.env.KEYDB_PORT;
 export const KEYDB_HOST: string | undefined = process.env.KEYDB_HOST;
 
-export const INFLUXDB_TOKEN: string | undefined = process.env.INFLUXDB_TOKEN
-export const INFLUXDB_ORG: string = `GMR Test`
-export const INFLUXDB_BUCKET: string = `Chat`
-
 export const KDB: { host: string | undefined; port: string | number; password: string | undefined } = {
   host: process.env.KEYDB_HOST,
   port: process.env.KEYDB_PORT || 6379,
@@ -38,17 +34,17 @@ export const KDB: { host: string | undefined; port: string | number; password: s
 // Thirdweb SDK Configuration
 export const SECRET_KEY: string = process.env.SECRET_KEY || ""
 // Chain and Wallet Factory Configuration
-export const CHAIN: typeof SkaleNebulaHubTestnet = SkaleNebulaHubTestnet
+export const CHAIN: typeof SkaleNebulaHubTestnet = SkaleNebulaHubTestnet;
 export const FACTORIES: Record<number, string> = { [SkaleNebulaHubTestnet.chainId]: "0x514f1d6B8d22911eE84f97eDececE0479e38E1b6" };
 // Contract Addresses
 export const BEATS_TOKEN: string = '0x63F8Cb0ffc1DeB782E84B9C68b2F85260fbd497d';
 export const THUMP_TOKEN: string = '0xDAd44595262D2390192762bbb3358DF807808480';
 export const KMR_TOKEN: string = '0xb6446F25afD9f18B9716Aa32d65df9370980d6c7';
 
-export const PACK_ADDRESS: string = '0x436B0468CF4d8dA8E887fA01F5F97d03C7A465e1';
-export const EDITION_ADDRESS: string = '0x09F143c0222505D7985482fCc7D3Abf7E3C987eA'; // ** NFT CARD ADDRESS
-export const CARD_MARKETPLACE: string = '0x7dc65A3EeBdFbCAC10C9f0a0ecaA62f98a8d1f00'; // ** CARD MARKETPLACE ADDRESS
-export const BUNDLE_MARKETPLACE: string = '0xcd5CB69Dcc7D52eAe5CD99Cc43F92F329507ED6E'; // ** LOOTBOX MARKETPLACE ADDRESS
+export const PACK_ADDRESS: string = '0x749bBCe456372c744e42a2391CFcb012E225ab65';
+export const EDITION_ADDRESS: string = '0x75E504b9B341CA7f75d330Fa916AC3FFD81c0B4c'; // ** NFT CARD ADDRESS
+export const CARD_MARKETPLACE: string = '0xE23DDfD5B6fCb662ED7afe90357C6f43c89E75Cc'; // ** CARD MARKETPLACE ADDRESS
+export const PACK_MARKETPLACE: string = '0x4E18FACbC9919C5C8fd596Ee358AbfDdb3c516a8'; // ** CARD PACK MARKETPLACE ADDRESS
 
 export const GOOGLE_CLIENT_ID: string  = process.env.GOOGLE_CLIENT_ID || ""
 export const GOOGLE_CLIENT_SECRET: string = process.env.GOOGLE_CLIENT_SECRET || ""
