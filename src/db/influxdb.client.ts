4ab02c491a6c72e79d55aaec980721920f73e0f3
YirenNing24 <paulanthonyarriola@gmail.com>
2024-04-22 19:34:37 +0800

    updates

--- src/db/influxdb.client.ts
+++ src/db/influxdb.client.ts
@@ -1,3 +1,32 @@
-import {InfluxDB, Point} from '@influxdata/influxdb-client'
+const {InfluxDB, Point} = require('@influxdata/influxdb-client')
 
-const influxDB = new InfluxDB({YOUR_URL, YOUR_API_TOKEN})
+
+const url = "http://localhost:8086"
+const token  = "s2rC7nAXmsmrpst_S4JnudkS06hSjqNzBaTgRFdxtGHwxMZv1-Jmt5hCSUf6UW1-kv1OHUofVfxTo9svqYKv-w=="
+
+const client = new InfluxDB({ url, token })
+
+let org = `GMETARAVE`
+let bucket = `CHAT`
+
+// let writeClient = client.getWriteApi(org, bucket, 'ns')
+
+let queryClient = client.getQueryApi(org)
+let fluxQuery = `from(bucket: "CHAT")
+ |> range(start: -10m)
+ |> filter(fn: (r) => r._measurement == "measurement1")`
+
+queryClient.queryRows(fluxQuery, {
+  next: (row: any, tableMeta: any) => {
+    const tableObject = tableMeta.toObject(row)
+    console.log(tableObject)
+  },
+  error: (error: any) => {
+    console.error('\nError', error)
+  },
+  complete: () => {
+    console.log('\nSuccess')
+  },
+})
+
+
