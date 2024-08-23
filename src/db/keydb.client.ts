233e2591e407477f8df1dcf68121c226ba2e32d4
YirenNing24 <paulanthonyarriola@gmail.com>
2024-08-24 03:32:45 +0800

    updates

--- src/db/keydb.client.ts
+++ src/db/keydb.client.ts
@@ -2,8 +2,8 @@ import { createClient } from 'redis';
 import { KEYDB_PASSWORD, HOST, KEYDB_PORT } from '../config/constants';
 
 // const keydb = createClient({ url:`redis://:${KEYDB_PASSWORD}@${HOST}:${KEYDB_PORT}`});
-const keydb = createClient({ url:`redis://${HOST}:6379`});
-console.log(`redis://${HOST}:${KEYDB_PORT}`)
+const keydb = createClient({ url:`redis://${HOST}:${KEYDB_PORT}`});
+
 
 keydb.on('error', err => console.log('KeyDB Client Error', err));
 
