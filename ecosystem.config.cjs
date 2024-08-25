d08abdcf8001bdbc3e11f61d8a1f9ff4d2b95daf
YirenNing24 <paulanthonyarriola@gmail.com>
2024-08-25 13:50:32 +0800

    updates

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
       script: "src/index.ts",
       interpreter: "/root/.bun/bin/bun",  // Use the absolute path
       env: {
-        NODE_TLS_REJECT_UNAUTHORIZED: 0
+        NODE_TLS_REJECT_UNAUTHORIZED: "0"
       },
       watch: true
     }
