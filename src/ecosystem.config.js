0e4fbf328af7c80246dfabe2f70699277bfffc5d
YirenNing24 <paulanthonyarriola@gmail.com>
2024-08-25 13:30:09 +0800

    updates

---
+++ src/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: "my-bun-app",
+        script: "src/index.ts",
+        interpreter: "~/.bun/bin/bun",
+        env: {
+          NODE_TLS_REJECT_UNAUTHORIZED: 0
+        },
+        watch: true
+      }
+    ]
+  };
+  
