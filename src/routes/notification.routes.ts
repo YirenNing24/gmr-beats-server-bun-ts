80a7389f6f4cf3dfd2fcfccf12f95e34dfb5d13f
YirenNing24 <paulanthonyarriola@gmail.com>
2024-08-29 00:14:47 +0800

    updates

---
+++ src/routes/notification.routes.ts
@@ -0,0 +1,34 @@
+//** ELYSIA IMPORT
+import Elysia from 'elysia'
+
+//** SERVICE IMPORT
+import NotificationService from '../game.services/notification.services/notification.service'
+
+//** TYPE INTERFACE IMPORT
+import { NotificationData } from '../game.services/notification.services/notification.interface';
+import { authorizationBearerSchema } from './route.schema/schema.auth';
+
+
+
+const notification = (app: Elysia): void => {
+    app.get('api/notification', async ({ headers }) => {
+      try {
+        const authorizationHeader: string | null = headers.authorization;
+        if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+          throw new Error('Bearer token not found in Authorization header');
+        }
+        const jwtToken: string = authorizationHeader.substring(7);
+
+        const notificationService: NotificationService = new NotificationService();
+        const output: NotificationData[] = await notificationService.getNotification(jwtToken);
+
+        return output;
+      } catch (error: any) {
+        throw error
+        }
+        }, authorizationBearerSchema
+    ) 
+    
+}
+  
+export default notification
